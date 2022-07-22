0701a6022976043e167cf93c4ad752f257f9c49f
Cristian Castillo <ccastillon92@gmail.com>
2022-07-22 20:17:59 +0200

    Revert "POC: add gtm"
    This reverts commit f9914e0052d490634b5e402f0c171c9f28cf14ee.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,53 +4,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head>
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-1D3M64DPVY" />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                dataLayer.push(arguments);
-              }
-              gtag('js', new Date());
-              gtag('config', 'G-1D3M64DPVY');
-            `
-            }}
-          />
-
-          {/* <!-- Google Tag Manager --> */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function (w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-              var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-N5L9DVV');`
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
-        </Head>
+        <Head />
 
         <body>
           <Main />
           <NextScript />
-
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-N5L9DVV"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
         </body>
       </Html>
     );
